8e57e495240734eff424a95c466dbd3c932f48e5
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-06-02 16:33:07 -0400

    init

---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+export default {
+    products:[{
+        _id: '1',
+        name: 'Cape May IPA',
+        category:'IPA',
+        image: '/images/CapeMayIPA.jpg',
+        price: 12,
+        brand: 'Cape May Brewery',
+        rating: 5.0,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'White Caps',
+        category: 'IPA',
+        image: '/images/Whitecaps.jpg',
+        price: 10,
+        brand: 'Cape May Brewery',
+        rating: 3.5,
+        numReviews: 10
+    },
+    {
+        _id: '3',
+        name: 'Coastal Evacuation',
+        category: 'IPA',
+        image: '/images/CoastalEvacuation.jpg',
+        price: 16,
+        brand: 'Cape May Brewery',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '4',
+        name: 'Bounding Man',
+        category: 'IPA',
+        image: '/images/BoundingMan.jpg',
+        price: 14,
+        brand: 'Cape May Brewery',
+        rating: 4.5,
+        numReviews: 10
+    },
+]
+}
---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import {logout, update} from '../actions/userActions';
+import {listMyOrders} from '../actions/orderActions';
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+
+function ProfileScreen(props){
+const [name, setName] = useState('');
+const [password, setPassword] = useState('');
+const [email, setEmail] = useState('');
+const dispatch = useDispatch();
+
+const userSignin = useSelector(state => state.userSignin);
+const {userInfo} = userSignin;
+const handleLogout = () => {
+    dispatch(logout());
+    props.history.push("/signin")
+}
+const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(update({userId: userInfo._id, email, name, password}))
+}
+
+const userUpdate = useSelector(state => state.userUpdate);
+const { loading, success, error} = userUpdate;
+
+const myOrderList = useSelector(state => state.myOrderList);
+const {loading: loadingOrders, orders, error: errorOrders} = myOrderList;
+
+useEffect(() => {
+    if(userInfo){
+        setEmail(userInfo.email);
+        setName(userInfo.name);
+        setPassword(userInfo.password);
+    }
+    dispatch(listMyOrders());
+    return () => {
+
+    };
+}, [])
+
+    return <div className="profile">
+        <div className="profile-info">
+        <div className="form">
+        <form onSubmit={submitHandler}>
+            <ul className="form-container">
+                <li>
+                    <h2>User Profile</h2>
+                </li>
+                <li>
+                    {loading && <div>Loading...</div>}
+                    {error && <div>{error}</div>}
+                    {success && <div>Profile updated successfully...</div>}
+                </li>
+                <li>
+                    <label htmlFor="name">
+                        Name
+                    </label>  
+                    <input value={name} type="name" name="name" id="name" onChange={(e) => setName(e.target.value)}>
+                    </input>
+                </li>
+                <li>
+                    <label htmlFor="email">
+                        Email
+                    </label>  
+                    <input value={email} type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}>
+                    </input>
+                </li>
+                <li>
+                    <label htmlFor="password">
+                        Password
+                     </label>
+                        <input value={password} type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}>
+                        </input>
+                </li>
+                <li>
+                    <button type="submit" className="button primary">Update</button>
+                </li>
+                <li>
+                    <button type="button" onClick={handleLogout} className="button secondary full-width">Logout</button>
+                </li>
+                
+            </ul>
+
+        </form>
+    </div>
+        </div>
+        <div className="profile-orders" className="content-margined">
+            {
+                loadingOrders? <div>Loading...</div>:
+                errorOrders? <div>{errorOrders}</div>:
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>Total</th>
+                        <th>Paid</th>
+                        <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map(order => <tr key={order._id}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt}</td>
+                                <td>{new Intl.NumberFormat("en-GB", {
+                            style: "currency",
+                            currency: "USD"
+                        }).format(order.totalPrice)}</td>
+                                <td>{order.isPaid}</td>
+                                <td>
+                                    <Link to={"/order/"+ order._id}>Details</Link>
+                                </td>
+                            </tr>)}
+                        </tbody>
+                </table>            }
+        </div>
+    </div>
+
+}
+export default ProfileScreen;
