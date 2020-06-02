9a400bb8a5b62852674849e6a66b2acecd070e70
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-06-02 16:44:17 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -35,4 +35,10 @@ app.get("/api/config/paypal", (req, res)=>{
 //    res.send(data.products);
 //});
 //*/
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
+
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
+app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
